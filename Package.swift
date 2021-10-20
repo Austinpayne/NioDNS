21cd4130e0d23b13d6ab9e899003373b3eb0ab16
Austin Payne <austin@passivelogic.com>
2021-10-20 17:42:36 -0600

    Loosen version requirement for swift-nio

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-nio.git", .exact("2.31.0")),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.31.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
